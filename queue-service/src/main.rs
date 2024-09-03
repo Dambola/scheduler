0d2e3996ef3c63f0eaca27ffc02259b4bd0b8295
Daniel N. Rebouças <danielnreboucas@hotmail.com>
2024-09-03 13:59:48 -0300

    removing docker files.

--- queue-service/src/main.rs
+++ queue-service/src/main.rs
@@ -1,6 +1,6 @@
-use std::net::SocketAddr;
-
 use config::Configuration;
+use database::get_postgres_pool;
+use queue::{insert_queued_job, CreateQueuedJob};
 
 mod config;
 mod database;
@@ -11,5 +11,18 @@ async fn main() {
     // Load the configurations
     let config = Configuration::new().expect("Error loading the configurations.");
     
+    let pool = get_postgres_pool(&config)
+        .await
+        .unwrap();
 
+    let new_job = CreateQueuedJob{
+        priority: 0,
+        task: "task".to_string(),
+        metadata: None,
+    };
+
+    let queued_job = insert_queued_job(&pool, new_job)
+        .await
+        .unwrap();
+    println!("{:?}", queued_job);
 }
--- queue-service/src/queue.rs
+++ queue-service/src/queue.rs
@@ -1,14 +1,14 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use sqlx::{Error, FromRow, PgPool};
+use sqlx::{Error, FromRow, PgPool, Row};
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
 pub struct CreateQueuedJob {
-    priority: i64,
-    task: String,
-    metadata: Option<Value>
+    pub priority: i64,
+    pub task: String,
+    pub metadata: Option<Value>
 }
 
 #[derive(Debug, FromRow, Deserialize, Serialize)]
@@ -20,65 +20,30 @@ pub struct QueuedJob {
     created_at: DateTime<Utc>
 }
 
-pub async fn push_to_queue(pool: &PgPool, queued_job: &CreateQueuedJob) -> Result<QueuedJob, Error> {
-    let query_result = sqlx::query_as!(
-        QueuedJob,
-        "INSERT INTO queue (priority, task, metadata) VALUES ($1, $2, $3) RETURNING id",
-        queued_job.priority,
-        queued_job.task.to_string(),
-        queued_job.metadata
-    )
-    .fetch_one(&pool)
-    .await;
+pub async fn insert_queued_job(pool: &PgPool, queued_job: CreateQueuedJob) -> Result<QueuedJob, Error> {
+    let query_result = sqlx::query(
+            "INSERT INTO queue (priority, task, metadata) VALUES ($1, $2, $3) RETURNING id",
+        )
+        .bind(queued_job.priority)
+        .bind(queued_job.task)
+        .bind(queued_job.metadata)
+        .fetch_one(pool)
+        .await;
 
     match query_result {
-        Ok(created_queued_job) => {
-            return Ok(created_queued_job);
+        Ok(row) => {
+            let id = row.try_get("id").expect("No 'id' found on PgRow");
+            get_queued_job_by_id(&pool, id).await
         }
-        Err(e) => {
-            return Err(e);
-        }
+        Err(e) => Err(e)
     }
 }
 
-impl QueueService {
-    pub async fn pop(client: &tokio_postgres::Client) -> Result<Option<QueuedJob>, Error> {
-        let row = client
-            .query_opt(
-                "DELETE FROM queue 
-                 WHERE id = (
-                     SELECT id 
-                     FROM queue 
-                     ORDER BY priority ASC, created_at ASC 
-                     LIMIT 1
-                 ) 
-                 RETURNING id, priority, task, metadata, created_at",
-                &[],
-            )
-            .await?;
-        
-        if let Some(row) = row {
-            Ok(Some(QueuedJob::from(row)))
-        } else {
-            Ok(None)
-        }
-    }
-
-    pub async fn find(client: &tokio_postgres::Client, task: String) -> Result<Vec<QueuedJob>, Error> {
-        let rows = client
-            .query(
-                "SELECT id, task, priority, metadata, created_at FROM queue WHERE task = $1", 
-                &[&task])
-            .await?;
-        
-        let result: Vec<QueuedJob> = rows.into_iter().map(QueuedJob::from).collect();
-        Ok(result)
-    }
-
-    pub async fn remove(client: &tokio_postgres::Client, task: String) -> Result<(), Error> {
-        client
-            .execute("DELETE FROM queue WHERE task = $1", &[&task])
-            .await?;
-        Ok(())
-    }
+pub async fn get_queued_job_by_id(pool: &PgPool, id: Uuid) -> Result<QueuedJob, Error> {
+    sqlx::query_as(
+            "SELECT * FROM queue WHERE id = $1",
+        )
+        .bind(id)
+        .fetch_one(pool)
+        .await
 }
