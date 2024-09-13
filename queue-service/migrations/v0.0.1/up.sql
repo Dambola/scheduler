3027c28fee8ea7564874ad4ca6a1903ceb982573
Daniel N. Rebouças <danielnreboucas@hotmail.com>
2024-09-13 08:13:18 -0300

    feat: removing rust code

--- queue-service/migrations/v0.0.1/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE queue (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    priority BIGINT NOT NULL,
-    parent UUID NOT NULL,
-    metadata JSONB NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
